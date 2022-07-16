4c4fe8ecb56e58ab55e63356f51573665b92ae37
mercedesvera1998 <108768147+mercedesvera1998@users.noreply.github.com>
2022-07-16 18:45:55 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+let numeroString1 = "10" + " ";
+let numeroString2 = "20" + " ";
+let numeroString3 = "30"; 
+let textoString4 = "Puntos:" + " ";
+let concatenaNumeros = textoString4 + numeroString1 + numeroString2 +  numeroString3;
+console.log (concatenaNumeros);
